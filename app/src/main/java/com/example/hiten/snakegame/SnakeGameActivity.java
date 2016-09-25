12983c9ed189e5f8d64b68a110ff57cb0e7841fa
HitenA <aikozsar@gmail.com>
2016-09-25 16:33:04 +0300

    Initial Test

---
+++ app/src/main/java/com/example/hiten/snakegame/SnakeGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.hiten.snakegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SnakeGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snake_game);
+    }
+}
